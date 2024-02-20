41dabcab993331f880a6994b988a2153cdd38010
RinatHM <156786772+RinatHM@users.noreply.github.com>
2024-02-20 15:05:47 +0500

    Ко второй задаче

--- Program.cs
+++ Program.cs
@@ -1,16 +1,30 @@
-﻿//Задача 1: Задайте значения M и N. 
-//Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
-//Использовать рекурсию, не использовать циклы.
-void RangeOfDigit(int n, int m )
+﻿//Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+
+int Ackermann(int m, int n)
 {
-    if (n >= m)
+    if (m == 0)
     {
-        Console.WriteLine(n);
-        return;
+        //Console.WriteLine($"if m=0, вернули n + 1");
+        return n + 1;
     }
-   
-    RangeOfDigit(n + 1, m);
-    Console.WriteLine(n);
+    else if (n == 0)
+    {
+        //Console.WriteLine($"if m!=0 n=0, вернули Ackermann(m - 1, 1)");
+        return Ackermann(m - 1, 1);
+    }
+    else
+    {
+        //Console.WriteLine($"if m!=0 n!=0, вернули Ackermann(m, n-1)");
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
 }
-//int n = 1;
-RangeOfDigit(1, 14);
+
+void Main()
+{
+    int m = 3;
+    int n = 3;
+    int result = Ackermann(m, n);
+    Console.WriteLine($"Ackermann({m}, {n}) = {result}");
+}
+Main();
