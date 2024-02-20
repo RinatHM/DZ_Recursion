7817589d9d966e3b13c1422bee8771aaedac78c7
RinatHM <156786772+RinatHM@users.noreply.github.com>
2024-02-20 15:04:44 +0500

    К первой задаче

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 1: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+void RangeOfDigit(int n, int m )
+{
+    if (n >= m)
+    {
+        Console.WriteLine(n);
+        return;
+    }
+   
+    RangeOfDigit(n + 1, m);
+    Console.WriteLine(n);
+}
+//int n = 1;
+RangeOfDigit(1, 14);
